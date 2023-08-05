73b167e568d2cbd4e0d0ec4b64e01c4399390d9f
SPP <songpon.ph@gmail.com>
2023-08-05 07:07:13 +0700

    SAVEPOINT

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build . -t digitalorganic/ubuntu-python:2004.3112
-docker push digitalorganic/ubuntu-python:2004.3112
+docker build . -t digitalorganic/ubuntu-python:2004.359
+docker push digitalorganic/ubuntu-python:2004.359
